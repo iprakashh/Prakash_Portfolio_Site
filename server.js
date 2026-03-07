e5ef6596c85a6c46338a8c873e5ee03372138629
iprakashh <pikugee99@gmail.com>
2026-03-07 14:55:00 +0530

    server fixed

--- server.js
+++ server.js
@@ -1,39 +1,49 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const nodemailer = require("nodemailer")
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 
-const app = express()
+const app = express();
 
-app.use(express.static(__dirname))
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/contact",(req,res)=>{
+// Homepage route
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-const transporter = nodemailer.createTransport({
-service:"gmail",
-auth:{
-user:"pikugee99@gmail.com",
-pass:"tgqo hhln civb qgon"
-}
-})
+// Contact form
+app.post("/contact", (req, res) => {
 
-const mailOptions={
-from:req.body.email,
-to:"pikugee99@gmail.com",
-subject:"Portfolio Contact",
-text:`Name: ${req.body.name}
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "pikugee99@gmail.com",
+      pass: "tgqo hhln civb qgon"
+    }
+  });
+
+  const mailOptions = {
+    from: req.body.email,
+    to: "pikugee99@gmail.com",
+    subject: "Portfolio Contact Message",
+    text: `
+Name: ${req.body.name}
 Email: ${req.body.email}
-Message: ${req.body.message}`
-}
+Message: ${req.body.message}
+`
+  };
 
-transporter.sendMail(mailOptions,function(error){
-if(error){
-res.send("Error sending message")
-}else{
-res.send("Message sent successfully")
-}
-})
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) {
+      res.send("Error sending message");
+    } else {
+      res.send("Message sent successfully");
+    }
+  });
 
-})
+});
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
